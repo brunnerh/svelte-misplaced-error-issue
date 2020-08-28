92bcf2b9dafcb34a2df4310a50eecce8ffd14a3c
Harald Brunner <brunnerh@users.noreply.github.com>
2020-08-28 03:14:52 +0200

    first commit

---
+++ src/components/input.svelte
@@ -0,0 +1,7 @@
+<script type="text/typescript">
+	export let unused = null;
+	export let unused2 = null;
+</script>
+
+<div class="root">
+</div>
---
+++ src/test.ts
@@ -0,0 +1,9 @@
+export class Test
+{
+	x = 1;
+
+	fun(arg)
+	{
+		console.log(arg);
+	}
+}
